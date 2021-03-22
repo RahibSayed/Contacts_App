9fda5cbb1f3795ab875a044b597a4d111f45d1c4
RahibSayed <50241553+RahibSayed@users.noreply.github.com>
2021-03-22 19:34:26 +0000

    Fixed query by name endpoint
    API endpoint query by name fixed, unit test for this endpoint adjusted to meet the required outcome. Comments added to unit tests.

--- Unit_tests/test_api.py
+++ Unit_tests/test_api.py
@@ -5,18 +5,22 @@ BASE_URL = 'http://localhost:5000'
 
 class TestApi(unittest.TestCase):
 
+    # test get all contacts
     def test_1_get_contacts(self):
         result = requests.get(BASE_URL + '/contact')
         self.assertAlmostEqual(result.status_code, 200)
     
+    # test get contact by id
     def test_2_get_contact(self):
         result = requests.get(BASE_URL + '/contact/1')
         self.assertAlmostEqual(result.status_code, 200)
 
+    # test get contact/s by name 
     def test_3_get_contact_by_name(self):
         result = requests.get(BASE_URL + '/contact/name?name=a')
         self.assertAlmostEqual(result.status_code, 200)
     
+    # test create contact
     def test_4_add_contact(self):
         contact = {
             "name": "Rahib",
@@ -27,6 +31,7 @@ class TestApi(unittest.TestCase):
         result = requests.post(BASE_URL + '/contact', json=contact)
         self.assertAlmostEqual(result.status_code, 201)
 
+    # test update contact
     def test_5_update_contact(self):
         contact = {
             "name": "Bob Change",
@@ -36,6 +41,7 @@ class TestApi(unittest.TestCase):
         result = requests.put(BASE_URL + '/contact/1', json=contact)
         self.assertAlmostEqual(result.status_code, 200)
 
+    # test delete contact
     def test_6_delete_contact(self):
         result = requests.delete(BASE_URL + '/contact/4')
         self.assertAlmostEqual(result.status_code, 204)
